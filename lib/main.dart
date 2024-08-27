14359f1b808a26e1d223811a82d7aa4a6ed444f4
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-08-27 23:08:16 +0500

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -7,13 +7,35 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+int count = 0;
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow,
       appBar: AppBar(
-        title: const Text('hello'),
+        backgroundColor: Colors.yellow[200],
+        title: const Text('Hello'),
       ),
+      body: Center(
+        child: Text(
+          '$count',
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber[600],
+          onPressed: () {
+            add();
+            setState(() {});
+          },
+          child: const Icon(Icons.plus_one)),
     );
   }
+}
+
+add() {
+  print('button pressed');
+  count++;
 }
